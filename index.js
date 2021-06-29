32efa53d3321704b6345d19324806a53bfb49a19
Lex Nelson <lexinelson597@gmail.com>
2021-06-28 20:14:09 -0400

    commit tree 6e3c7166a5e7d0a858117311922f1bf7e765a01a

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Code your solution in this file!
+const headQ = 42
+
+
+function distanceFromHqInBlocks(blocks) {
+   if (blocks > 42){
+       return (blocks - 42);
+   } else {
+       return (42 - blocks);
+   }
+}
+
+function distanceFromHqInFeet(blocks) {
+    return (distanceFromHqInBlocks(blocks) * 264);
+}
+function distanceTravelledInFeet(blockA, blockB){
+    if (blockA > blockB) {
+        return ((blockA - blockB) * 264);
+    } else return ((blockB - blockA) * 264);
+}
+function calculatesFarePrice(blockA, blockB) {
+    if (distanceTravelledInFeet(blockA, blockB) < 400) {
+        return 0
+    } else if (distanceTravelledInFeet(blockA, blockB) < 2000) {
+        return ((distanceTravelledInFeet(blockA, blockB) - 400) * .02 )
+    } else if (distanceTravelledInFeet(blockA, blockB) < 2500){
+        return 25
+    } else if (distanceTravelledInFeet(blockA, blockB) > 2500) {
+        return 'cannot travel that far'
+    }
+}
